28709c773076d047d404b0519e68aa1b77692425
Swadesh Behera <itswadesh@gmail.com>
2024-02-19 15:18:47 +0530

    -

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
-const sumaDos = (arg1: number, arg2: number): number => {
-  /* TODO: should check if both are numbers */
-  return arg1 + arg2;
+const swadesh = (arg1: number, arg2: number): number => {
+  return arg1 + arg2
 }
 
-export {
-  sumaDos
-}
+export { swadesh }

f4385f1dff4067de11024cf6f58eec01252fce99
Swadesh Behera <itswadesh@gmail.com>
2024-02-19 15:54:55 +0530

    -

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -298,30 +298,6 @@ export const navigateToProperPath = (url: string) => {
   // }
 }
 
-export const shopifyInit = async ({ query, variables }: any) => {
-  try {
-    // @ts-ignore
-    const result = await fetch(import.meta.env.VITE_SHOPIFY_API_ENDPOINT, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        // @ts-ignore
-        'X-Shopify-Storefront-Access-Token': import.meta.env
-          .VITE_SHOPIFY_STOREFRONT_API_TOKEN
-      },
-      body: JSON.stringify({ query, variables })
-    }).then((res) => res.json())
-    if (result.errors) {
-      // console.log({ errors: result.errors })
-    } else if (!result || !result.data) {
-      // console.log({ result })
-      return 'No results found.'
-    }
-    return result.data
-  } catch (error) {
-    console.error(error)
-  }
-}
 export * from './string'
 export * from './api'
 export * from './validations'

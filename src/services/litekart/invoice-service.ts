a662ba9a0aba5d4e1669386a5b2288227d5705a7
ritikranjan12 <ritik123453@gmail.com>
2024-05-31 20:11:49 +0530

    new api added

--- src/services/litekart/invoice-service.ts
+++ src/services/litekart/invoice-service.ts
@@ -27,3 +27,17 @@ export const fetchAllInvoices = async ({ query = '', isCors = false, origin = nu
 
 	return res || {}
 }
+
+export const fetchAllRetailerInvoices = async ({ query = '', isCors = false, origin = null, sid = null, storeId }) => {
+	let res = {}
+
+	// if (isServer || isCors) {
+	// 	res = await getBySid(`get-all-invoices?store=${storeId}&${query}`, sid)
+	// } else {
+	// 	res = await getAPI(`get-all-invoices?store=${storeId}&${query}`, origin)
+	// }
+
+	res = await getAPI(`/api/invoices-of-retailer?store=${storeId}`, origin, sid)
+
+	return res || {}
+}

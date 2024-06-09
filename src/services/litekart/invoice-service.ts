d6588799de3978b49340613b24719dcd4974d4b7
Swadesh Behera <itswadesh@gmail.com>
2024-06-09 12:06:55 +0530

    invoices

--- src/services/litekart/invoice-service.ts
+++ src/services/litekart/invoice-service.ts
@@ -1,43 +1,53 @@
-import { getAPI, post, put } from './../../utils/api'
+import { getAPI } from './../../utils/api'
 
-export const fetchInvoices = async ({ query = '', isCors = false, origin = null, sid = null, storeId }) => {
-	let res = {}
+export const fetchInvoices = async ({
+  query = '',
+  isCors = false,
+  origin = null,
+  sid = null,
+  storeId
+}) => {
+  let res = {}
 
-	// if (isServer || isCors) {
-	// 	res = await getBySid(`get-orders-invoices?store=${storeId}&${query}`, sid)
-	// } else {
-	// 	res = await getAPI(`get-orders-invoices?store=${storeId}&${query}`, origin)
-	// }
+  res = await getAPI(
+    `get-orders-invoices?store=${storeId}&${query}`,
+    origin,
+    sid
+  )
 
-	res = await getAPI(`get-orders-invoices?store=${storeId}&${query}`, origin, sid)
-
-	return res || {}
+  return res || {}
 }
 
-export const fetchAllInvoices = async ({ query = '', isCors = false, origin = null, sid = null, storeId }) => {
-	let res = {}
+export const fetchAllInvoices = async ({
+  query = '',
+  isCors = false,
+  origin = null,
+  sid = null,
+  storeId
+}) => {
+  let res = {}
 
-	// if (isServer || isCors) {
-	// 	res = await getBySid(`get-all-invoices?store=${storeId}&${query}`, sid)
-	// } else {
-	// 	res = await getAPI(`get-all-invoices?store=${storeId}&${query}`, origin)
-	// }
+  res = await getAPI(`get-all-invoices?store=${storeId}&${query}`, origin, sid)
 
-	res = await getAPI(`get-all-invoices?store=${storeId}&${query}`, origin, sid)
-
-	return res || {}
+  return res || {}
 }
 
-export const fetchAllRetailerInvoices = async ({ query = '', isCors = false, origin = null, sid = null, storeId }) => {
-	let res = {}
+export const fetchAllRetailerInvoices = async ({
+  query = '',
+  isCors = false,
+  origin = null,
+  sid = null,
+  storeId
+}) => {
+  let res = {}
 
-	// if (isServer || isCors) {
-	// 	res = await getBySid(`get-all-invoices?store=${storeId}&${query}`, sid)
-	// } else {
-	// 	res = await getAPI(`get-all-invoices?store=${storeId}&${query}`, origin)
-	// }
+  // if (isServer || isCors) {
+  // 	res = await getBySid(`get-all-invoices?store=${storeId}&${query}`, sid)
+  // } else {
+  // 	res = await getAPI(`get-all-invoices?store=${storeId}&${query}`, origin)
+  // }
 
-	res = await getAPI(`invoices-of-retailer?store=${storeId}`, origin, sid)
+  res = await getAPI(`invoices-of-retailer?store=${storeId}`, origin, sid)
 
-	return res || {}
+  return res || {}
 }
